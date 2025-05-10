033357a6c8751d46184f01cdd9be0849cbe0e01c
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2025-05-10 11:51:39 +0330

    Add Rating component in home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 //! Box Icons
-import "boxicons/css/boxicons.mini.css";
+import "boxicons/css/boxicons.min.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+//! Components
+import Rating from "@/components/Rating";
 
 export default function Home() {
   return (
-    <div></div>
+    <div className="font-kanit">
+      <Rating />
+    </div>
   );
 }
